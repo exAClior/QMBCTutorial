00073859d6b7ecfa4517a63fdddab58f3c573896
exAClior <yushengzhao2020@outlook.com>
2023-08-03 20:31:49 +0800

    start tutorial notebook

---
+++ notebooks/1.tutorial.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.27
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 57684dc8-31f9-11ee-2888-770b687183aa
+
+
+# ╔═╡ Cell order:
+# ╠═57684dc8-31f9-11ee-2888-770b687183aa
